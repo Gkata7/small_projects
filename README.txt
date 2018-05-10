33ebd386eedd8d34690a9eae8782e00c7b731d4f
Garrett Katashima <gkatashima@gmail.com>
2018-05-10 00:05:47 -0700

    updated README

--- README.txt
+++ README.txt
@@ -1,5 +1,4 @@
 Created some small projects using HTML, CSS, JavaScript, JQuery, Bootstrap, Font Awesome.
 
 1. Random Quotes:
-Link:
 http://quote_random.surge.sh/
--- show_weather/javascript/script.js
+++ show_weather/javascript/script.js
@@ -1,34 +1,7 @@
-// $(document).ready(function(){
-//   var long;
-//   var lat;
-//   var temp;
-//   // To get location of weather(longitude and latitude)
-//   if(navigator.geolocation){
-//     navigator.geolocation.getCurrentPosition(function(position){
-//       long = "long=" + position.coords.longitude;
-//       lat = "lat=" + position.coords.latitude;
-//       $("#data").html("latitude: " + lat + "<br>longitude: " + long);
-//     });
-//   }
-//
-//   var api = "http://api.openweathermap.org/data/2.5/weather?lat=:lat+&lon=:long&appid=06ca135e1b172b2bc2dfe6db7cf7b952";
-//   // var api = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=06ca135e1b172b2bc2dfe6db7cf7b952";
-//
-//   $.getJSON(api, function(data){
-//     var weatherType = data.weather[0].description;
-//     var kelvin = data.main.temp;
-//     var windspeed = data.wind.speed;
-//     var city = data.name;
-//
-//     console.log(city);
-//     console.log(api);
-//   });
-// });
-
 $(document).ready(function(){
   var fahrenheit,celsius;
   var weatherUrl = "https://api.openweathermap.org/data/2.5/weather";
-  var apiKey = API KEY;
+  var apiKey = "06ca135e1b172b2bc2dfe6db7cf7b952";
   // getLatLong function will a call to geoip api to recieve user's location.
   getLatLong();
 
@@ -36,14 +9,14 @@ $(document).ready(function(){
   function getLatLong(){
     // .ajax() function takes in a url, type, dataType, success:
     $.ajax({
-      url: "https://geoip-db.com/json", // url = end point of the URL from the api
+      url: "https://geoip-db.com/json/", // url = end point of the URL from the api
       type: "GET", // type = It's a get post, to recieve info
       dataType: "json", // dataType = data recieved from the Ajax call.
       success: function(data){ // success = what spits out when the search is successful
-        var lat = data.latitude;
+        var lat;
         var long = data.longitude;
         $(".city").html(data.city);
-        $(".country").html(data.country);
+        $(".country").html(data.country_name);
         weatherUrl += "?lat="+lat+"&lon="+long+"&APPID="+apiKey+"&units=metric";
         getWeather();
       },
@@ -65,10 +38,10 @@ $(document).ready(function(){
         celsius = tempature;
         fahrenheit = celsius*1.8+32;
         var icon = data.weather[0].icon;
-        var weatherDetail = data.weather[0].main+" , "+data.weather[0].description;
-        $(".weatherDetail").html(weatherDetail); // updates weathe description
-        $(".icon").attr("src", "http://openweathermap.org/img/w/'+icon+'.png"); // update the icon based on weather location
-        $(".temp").html(tempature+"&#8451"); // updates the tempature
+        var weatherDetail = data.weather[0].main+", "+data.weather[0].description;
+        $(".weatherDetail").html(weatherDetail); // updates weather description
+        $(".iconPic").attr("src", "http://openweathermap.org/img/w/"+icon+".png"); // update the icon based on weather location
+        $(".temp").html(tempature+"&#8451;"); // updates the tempature
       },
       error: function(err){
         alert("Something isn't right, please try again");
@@ -76,5 +49,15 @@ $(document).ready(function(){
       }
     });
   }
+
+  $(".toggle .btn").click(function(){
+    if($(".toggle").attr("id") == "c"){
+      $(".temp").html(fahrenheit+"&#8457;");
+      $(".toggle").attr("id", "f");
+    } else if($(".toggle").attr("id") == "f"){
+      $(".temp").html(celsius+"&#8451;");
+      $(".toggle").attr("id", "c");
+    }
+  });
 
 });
